733102bbdf938549ee14b4c76fbebd99d6929f12
sl <sl.smirn.2204@gmail.com>
2019-03-12 11:18:00 +0700

    controller using web.xml

--- src/main/java/ru/smirnovv/HelloSpringController.java
+++ src/main/java/ru/smirnovv/HelloSpringController.java
@@ -1,13 +1,13 @@
-package ru.smirnovv.controller;
+package ru.smirnovv;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+@RequestMapping("/")
 public class HelloSpringController {
-    @GetMapping(value = "/index")
+    @GetMapping
     public String spring() {
         return "Hello Spring";
     }
